7a03725aa09beb273d449d1c2b5bfab3ee6bd547
mauriciogofas <mauricio@gofas.net>
2023-04-04 01:15:11 -0300

    v1.2.0

--- modules/gateways/gofasiuguboleto/custom/index.php
+++ modules/gateways/gofasiuguboleto/custom/index.php
@@ -1,9 +1,10 @@
 <?php
 /**
  * Módulo iugu Boleto para WHMCS
- * @copyright	2022 Gofas Software
+ * @copyright	2023 Gofas Software
  * @see			https://gofas.net/?p=14942
  * @license		https://gofas.net/?p=9340
  * @support		https://gofas.net/?p=14687
- * @version		0.1.0
+ * @version		1.2.0
  */
+// Silence is golden
